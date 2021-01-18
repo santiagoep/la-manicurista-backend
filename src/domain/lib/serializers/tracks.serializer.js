decc2e446450008c7e6c58542888d4a44d294306
santiagoep <hola@santiagoep.com>
2021-01-18 02:35:05 -0500

    Solving ecma conflict

--- src/domain/lib/serializers/tracks.serializer.js
+++ src/domain/lib/serializers/tracks.serializer.js
@@ -10,11 +10,11 @@ const serializeTracks = (tracks) =>
     name,
     imgPreviews: {
       small: {
-        src: album?.images[2]?.url,
+        src: album.images[2].url,
         alt: name
       },
-      medium: { src: album?.images[1]?.url, alt: name },
-      large: { src: album?.images[0]?.url, alt: name }
+      medium: { src: album.images[1].url, alt: name },
+      large: { src: album.images[0].url, alt: name }
     },
     preview,
     artists: serializeArtists(artists)
